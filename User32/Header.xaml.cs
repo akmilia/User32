3bdd501621f2fbddcd67cc196c4ee03d8efc380c
User29 <user29@local.ru>
2025-04-23 12:24:41 +0300

    Добавьте файлы проекта.

---
+++ User32/Header.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace User32
+{
+    /// <summary>
+    /// Логика взаимодействия для Header.xaml
+    /// </summary>
+    public partial class Header : UserControl
+    {
+        public Header()
+        {
+            InitializeComponent();
+        }
+        private void transport_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем доступ к Frame из MainWindow
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.MainFrame.Navigate(new TransportWindow());
+        }
+
+        private void clients_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.MainFrame.Navigate(new ClientWindow());
+        }
+
+        private void discount_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.MainFrame.Navigate(new DiscountWindow());
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); // Корректное закрытие приложения
+        }
+
+        private void manager_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.MainFrame.Navigate(new ManagerWindow());
+        }
+    }
+}
---
+++ User32/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+namespace User32
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User32Entities entities = new User32Entities(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new TransportWindow());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var login = Login.Text;
+                var pass = Password.Password;
+
+                var user = entities.Manager.FirstOrDefault(u => u.login == login && u.password == pass);
+
+                bool check = Valid(login, pass);
+                if (!check) { return; }
+
+                if (user != null)
+                {
+                    MainFrame.Navigate(new ManagerWindow());
+                }
+
+                else MessageBox.Show("User not found");
+            }
+            catch
+            {
+                MessageBox.Show("try later");
+            }
+        }
+
+        public bool Valid(string login, string pas)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(pas))
+    {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return false;
+            }
+
+            if (login.Length > 100 ||  pas.Length > 100)
+    {
+                MessageBox.Show("Слишком много букв");
+                return false;
+            }
+
+            return true;
+
+        }
+    } 
+    }
